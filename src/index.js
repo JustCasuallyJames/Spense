ea6208d63e0bbf35cb568c769eb45f2bf562ce96
James Vo <justcasuallyjames@gmail.com>
2022-02-26 09:55:50 -0800

    modifying the index file

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: "AIzaSyC71Yj2cu-br9eAJ0e4G71sXCMaz3GkY8I",
     authDomain: "xpense-400b7.firebaseapp.com",
     projectId: "xpense-400b7",
     storageBucket: "xpense-400b7.appspot.com",
     messagingSenderId: "654131006558",
-    appId: "1:654131006558:web:7411e494e6c238a922f8cf"
+    appId: "1:654131006558:web:7411 e494e6c238a922f8cf"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
+export default db;
